bd969d652fbb1d2b24f0af77714f50f8fa56907c
Andrew Dunn <atd2005@gmail.com>
2024-10-16 18:05:00 -0500

    update welcome

--- src/web-ui/src/public/Welcome.vue
+++ src/web-ui/src/public/Welcome.vue
@@ -1,16 +1,26 @@
 <template>
-  <Layout :showNav="false" :showFooter="false" :showTextAlerts="false" :showDemoGuide="false" backgroundColor="var(--aws-squid-ink)">
+  <Layout
+    :showNav="false"
+    :showFooter="false"
+    :showTextAlerts="false"
+    :showDemoGuide="false"
+    backgroundColor="var(--aws-squid-ink)"
+  >
     <div class="container mb-2 text-left">
-      <h1 class="heading my-5 text-center">Welcome to the Retail Demo Store!</h1>
+      <h1 class="heading my-5 text-center">
+        Welcome to the Jigsaw Demo Store!
+      </h1>
 
       <p>
-        The Retail Demo Store is a demo platform to demonstrate how AWS infrastructure and services can be used to build
-        personalized customer experiences across multiple channels of engagement.
+        The Retail Demo Store is a demo platform to demonstrate how AWS
+        infrastructure and services can be used to build personalized customer
+        experiences across multiple channels of engagement.
       </p>
 
       <p>
-        The Retail Demo Store mimics an online store with products, users and user-interactions to showcase the
-        personalized product recommendations a shopper would receive based on historical and real time click-through
+        The Retail Demo Store mimics an online store with products, users and
+        user-interactions to showcase the personalized product recommendations a
+        shopper would receive based on historical and real time click-through
         behavior.
       </p>
 
@@ -20,34 +30,51 @@
         <ul
           class="service-list mb-0 pl-0 flex-grow-1 d-flex flex-column flex-lg-row justify-content-lg-around font-weight-bold"
         >
-          <li v-for="service in services" :key="service.text" class="mb-2 mb-lg-0">
-            <ServiceBadge :text="service.text" :src="service.src"></ServiceBadge>
+          <li
+            v-for="service in services"
+            :key="service.text"
+            class="mb-2 mb-lg-0"
+          >
+            <ServiceBadge
+              :text="service.text"
+              :src="service.src"
+            ></ServiceBadge>
           </li>
         </ul>
       </div>
 
-      <div class="mt-2 mb-4 my-sm-5 d-flex flex-column align-items-center align-items-sm-end">
+      <div
+        class="mt-2 mb-4 my-sm-5 d-flex flex-column align-items-center align-items-sm-end"
+      >
         <div class="d-flex flex-column flex-sm-row align-items-center">
-          <div class="login-cta d-flex justify-content-center align-items-center">
-            Have an account?<router-link to="/auth" class="sign-in btn btn-link">Sign in</router-link>
+          <div
+            class="login-cta d-flex justify-content-center align-items-center"
+          >
+            Have an account?<router-link to="/auth" class="sign-in btn btn-link"
+              >Sign in</router-link
+            >
           </div>
-          <router-link to="/auth?signup=true" class="create-account mt-3 mt-sm-0 ml-sm-3 btn btn-primary"
+          <router-link
+            to="/auth?signup=true"
+            class="create-account mt-3 mt-sm-0 ml-sm-3 btn btn-primary"
             >Create an account</router-link
           >
         </div>
 
-        <router-link to="/" class="mt-3 skip-login btn btn-link">Skip login for now</router-link>
+        <router-link to="/" class="mt-3 skip-login btn btn-link"
+          >Skip login for now</router-link
+        >
       </div>
     </div>
   </Layout>
 </template>
 
 <script>
-import Layout from '@/components/Layout/Layout.vue'
-import ServiceBadge from './components/ServiceBadge.vue';
+import Layout from "@/components/Layout/Layout.vue";
+import ServiceBadge from "./components/ServiceBadge.vue";
 
 export default {
-  name: 'Welcome',
+  name: "Welcome",
   components: {
     Layout,
     ServiceBadge,
@@ -56,16 +83,16 @@ export default {
     return {
       services: [
         {
-          text: 'Amazon Personalize',
-          src: '/personalize.svg',
+          text: "Amazon Personalize",
+          src: "/personalize.svg",
         },
         {
-          text: 'Amazon Pinpoint',
-          src: '/pinpoint.svg',
+          text: "Amazon Pinpoint",
+          src: "/pinpoint.svg",
         },
         {
-          text: 'Amazon Lex',
-          src: '/lex.svg',
+          text: "Amazon Lex",
+          src: "/lex.svg",
         },
       ],
     };
